b2240bc894bebfde3d65f2f93ff7b2fe14fe85e1
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-01-07 20:55:54 +0530

    Time: 1 ms (40.70%), Space: 39.3 MB (14.52%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,26 +1,31 @@
 class Solution {
     public double power(double x, long n){
-        if(n<=0){
+        if(n==0){
             return 1.0;
         }
         else
             if(n==1){
                 return (double)x;
             }
+        double ans=power(x,n/2);
         if(n%2==0){
-            double ans=power(x,n/2);
+            // 2^4= 2^2*2^2;
+           
             return ans*ans;
         }
        else{
-           double ans1=power(x,n/2);
-           return x*ans1*ans1;
+          
+           return x*ans*ans;
        } 
         
     }
     public double myPow(double x, int n) {
           if(n<0){
+              //Reason to take long 
+              //Maximum negative value of Integer cannot use in the normal format.
+              
               long temp=n;
-            //  System.out.println(Math.abs(temp));
+       
               return 1/power(x,Math.abs(temp));
           }
          else{
