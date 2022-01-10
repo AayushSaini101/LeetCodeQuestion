402522c57768e848476821ecdff25652cd77da76
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-01-10 17:38:42 +0530

    Time: 301 ms (50.00%), Space: 180.5 MB (12.50%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.java
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.java
@@ -1,32 +1,78 @@
 class Solution {
-    public char[] sort(char ch[]){
-        Arrays.sort(ch);
-        return ch;
-    }
-   public int wordCount(String[] startWords, String[] targetWords) {
-     List<String>sortedTargetWords=new ArrayList<>();
-     HashSet<String>H=new HashSet<>();
-     for(String elements:startWords){
-         H.add(new String(sort(elements.toCharArray())));
-     }
-     for(String elements:targetWords){
-         sortedTargetWords.add(new String(sort(elements.toCharArray())));
-     }
-        int count=0;
-        for(int i=0;i<sortedTargetWords.size();++i){
-            StringBuffer string=new StringBuffer(sortedTargetWords.get(i));
-            for(int j=0;j<string.length();++j){
-                //removing every word and checking 
-                StringBuffer temp=new StringBuffer(string.substring(0,j)).append(string.substring(j+1,string.length()));
-               
-                if(H.contains(temp.toString())){// System.out.println(temp);
-                    ++count;
+    public int wordCount(String[] startWords, String[] targetWords) {
+        Trie trie = new Trie();
+        
+        for (String word: startWords) {
+            int len = word.length();
+            int[] count = new int[26];
+            
+            for (int i = 0; i < len; ++ i) {
+                count[word.charAt(i) - 'a'] = 1;
+            }
+            
+            trie.insert(count);
+        }
+        
+        int output = 0;
+        for (String word: targetWords) {
+            int len = word.length();
+            
+            int[] count = new int[26];
+            for (int i = 0; i < len; ++ i) {
+                count[word.charAt(i) - 'a'] = 1;
+            }
+            
+            for (int i = 0; i < 26; ++ i) {
+                if (count[i] == 0) {
+                    continue;
+                }
+                count[i] = 0;
+                if (trie.search(count)) {
+                    ++ output;
                     break;
                 }
+                count[i] = 1;
             }
         }
-          return count;
+        
+        return output;
     }
-  
-  
+    
+    class Trie {
+        TrieNode root;
+        
+        Trie() {
+            root = new TrieNode();
+        }
+        
+        void insert(int[] arr) {
+            TrieNode ptr = root;
+            for (int i = 0; i < 26; ++ i) {
+                if (ptr.children[arr[i]] == null) {
+                    ptr.children[arr[i]] = new TrieNode();
+                }
+                ptr = ptr.children[arr[i]];
+            }
+        }
+        
+        boolean search(int[] arr) {
+            TrieNode ptr = root;
+            for (int i = 0; i < 26; ++ i) {
+                if (ptr.children[arr[i]] == null) {
+                    return false;
+                }
+                ptr = ptr.children[arr[i]];
+            }
+            
+            return true;
+        }
+    }
+    
+    class TrieNode {
+        TrieNode[] children;
+        
+        TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
 }
