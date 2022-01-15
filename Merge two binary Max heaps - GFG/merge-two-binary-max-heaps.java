3fed0d003ae7107bad8a48b1a8c3c95068544c85
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-01-15 11:35:42 +0530

    Added solution - LeetHub

---
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.java
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+    public static boolean isMerged(int[] a, int[] b, int[] merged){
+        if(a.length + b.length != merged.length)
+            return false;
+        
+        int[] m = new int[merged.length];
+        int i = 0;
+        for(int e : a)
+            m[i++] = e;
+        for(int e : b)
+            m[i++] = e;
+        Arrays.sort(m);
+        int[] mergedCopy = Arrays.copyOf(merged, merged.length);
+        Arrays.sort(mergedCopy);
+        if(!Arrays.equals(m, mergedCopy))
+            return false;
+        
+        for(i = 1; i < merged.length; i++){
+            if(merged[i] > merged[(i-1)/2])
+                return false;
+        }
+        
+        return true;
+    }
+    
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int a[] = new int[n];
+            int b[] = new int[m];
+            int merged[];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            int[] copyA = Arrays.copyOf(a, a.length), copyB = Arrays.copyOf(b, b.length);
+            merged = obj.mergeHeaps(a, b, n, m);
+            
+            boolean flag = isMerged(copyA, copyB, merged);
+            System.out.println(flag ? 1 : 0);
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int[] mergeHeaps(int[] a, int[] b, int n, int m) {
+        // your code here
+        PriorityQueue<Integer>P=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<a.length;++i){
+            P.add(a[i]);
+        }
+        for(int i=0;i<b.length;++i){
+            P.add(b[i]);
+        }
+        int ans[]=new int[a.length+b.length];
+        Iterator<Integer>it=P.iterator();
+        int index=0;
+        while(it.hasNext()){
+            ans[index++]=it.next();
+        }
+        return ans;
+    }
+}
