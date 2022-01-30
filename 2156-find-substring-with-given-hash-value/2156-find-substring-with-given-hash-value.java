cde3991fed1a12f4bc55c417bc02aa69f72b1822
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-01-30 09:36:02 +0000

    Time: 693 ms (14.29%), Space: 42.6 MB (28.57%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.java
@@ -1,33 +1,39 @@
 class Solution {
-  public String subStrHash(String s, int power, int modulo, int k, int hashValue) {
-        long[] p = new long[k]; //to store all the power values beforehand to avoid redundant calculations
-        for(int i=0;i<k;i++){
-            p[i] = binpow(power, i, modulo);
+    long  power(long a,long b,long mod){
+    a%=mod;
+    long res=1;
+    while(b>0){
+        if(b%2==1){
+            res=(res*a)%mod;
         }
-
-        for(int j=0;j<s.length()-k+1;j++){
+        a=(a*a)%mod;
+        b>>=1;
+    }
+    return res;
+}
+   
+    public String subStrHash(String s, int power1, int mod, int k, int hashValue) {
+    
+     long power[]=new long[k];
+     
+     for(int i=0;i<power.length;++i){
+         power[i]=power(power1,i,mod);
+     }
+        //We need to find the value of the opiw
+     
+        
+        for(int j=0;j<=s.length()-k;j++){
             long hash = 0;
-            for(int i=j;i<j+k;i++){
-                hash += (s.charAt(i)-'a'+1)*p[i-j];
+            for(int i=0;i<k;i++){
+                hash += (s.charAt(i+j)-'a'+1)*power[i];
             }
-            hash %= modulo;
+            hash %= mod;
             if(hash==hashValue)
                 return s.substring(j,j+k);
         }
         return "";
     }
+           
     
-    // function to calculate (a^n)mod(n) in O(log(n)) time
-    private long binpow(long a, long b, long m) {
-        a %= m;
-        long res = 1;
-        while (b > 0) {
-            if (b%2==1)
-                res = res * a % m;
-            a = a * a % m;
-            b >>= 1;
-        }
-        return res;
-    }
 
 }
