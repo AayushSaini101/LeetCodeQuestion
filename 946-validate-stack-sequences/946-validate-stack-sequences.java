b310c0d9b7e5e70ee97c4f579b2c645fe7c5b0da
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-03-16 10:05:31 +0530

    Time: 16 ms (5.64%), Space: 45.2 MB (37.70%) - LeetHub

---
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer>S=new Stack<>();
+        int index1=0;
+        int index2=0;
+        while(index1<pushed.length&&index2<popped.length){
+            if(S.size()!=0&&popped[index2]==S.peek()){
+                //Removing the element
+                S.pop();
+                
+                ++index2;
+            }
+            else{
+                if(S.contains(popped[index2])==false){
+                   S.add(pushed[index1]);
+                   
+                    ++index1;
+                    
+                   
+                }
+                else{
+                    break;
+                }
+            
+            }
+         
+        }
+        
+        
+        while(index2<popped.length){
+            if(S.peek()!=popped[index2]){
+                break;
+            }
+            S.pop();
+           ++index2;
+        }
+        
+        if(S.size()!=0){
+            return false;
+        }
+        return true;
+        
+    }
+}
