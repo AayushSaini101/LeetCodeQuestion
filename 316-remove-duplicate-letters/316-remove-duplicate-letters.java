9b601bbed1b4db63af9412ba10eddeceea4ed0e0
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-03-18 13:17:03 +0530

    Time: 10 ms (19.55%), Space: 44 MB (6.38%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -0,0 +1,45 @@
+class Solution {
+  
+    public String removeDuplicateLetters(String s) {
+        
+     LinkedHashMap<Character,Integer>H=new LinkedHashMap<>();
+     for(int i=0;i<s.length();++i){
+         char ch=s.charAt(i);
+         H.put(ch,H.getOrDefault(ch,0)+1);
+     }
+       // System.out.println(H);
+        StringBuilder ans=new StringBuilder("");
+        Stack<Character>S=new Stack<>();
+        
+        for(int i=0;i<s.length();++i){
+              if(S.contains(s.charAt(i))){
+                  if(H.get(s.charAt(i))>1){
+                      H.put(s.charAt(i),H.get(s.charAt(i))-1);
+                  }
+                  continue;
+              }
+           while(S.size()!=0&&S.peek()>=s.charAt(i)){
+               //We can pop the S.peek() only when the frequency is greater than 1
+                char ch=S.peek();
+                if(H.get(ch)==1){
+                    //Don't remove
+                    break;
+                }
+                else{
+                    S.pop();
+                    H.put(ch,H.get(ch)-1);
+                }
+           }
+         //  H.put(s.charAt(i),H.get(s.charAt(i))-1);
+           S.add(s.charAt(i));
+              
+            
+        }
+        
+        while(S.size()!=0){
+            ans.append(S.pop());
+        }
+        ans.reverse();
+        return ans.toString();
+    }
+}
