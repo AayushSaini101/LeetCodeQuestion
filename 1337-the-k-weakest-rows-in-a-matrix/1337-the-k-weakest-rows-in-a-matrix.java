dcb9ac0ba5ba2a9d28365377f223a029fc900095
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-03-27 23:04:48 +0530

    Time: 2 ms (88.13%), Space: 49.7 MB (7.87%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,42 @@
+class Solution {
+    public class Pair{
+        int index;
+        int count;
+        Pair(int index,int count){
+            this.index=index;
+            this.count=count;
+        }
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        //Create an array of size no. of rows present in mat
+        int arr[] = new int[rows];
+        for(int i = 0; i < rows; i++){
+            int count1 = 0;
+            for(int j = 0; j < cols; j++){
+                if(mat[i][j] == 1){
+                    count1++;
+                }
+            }
+            arr[i] = count1;
+        }   
+        
+        int ans[] = new int[k];//Create ans array of size k
+        for(int i = 0; i < k; i++){
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[j] < min){
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            ans[i] = minIndex;
+            arr[minIndex] = Integer.MAX_VALUE;
+        }
+        
+        return ans;
+        
+    }
+}
