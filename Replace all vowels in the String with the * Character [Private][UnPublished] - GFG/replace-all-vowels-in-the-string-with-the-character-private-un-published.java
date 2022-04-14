0282542beb7728577a152079e6a5e848c8ddf3a4
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-04-14 21:03:16 +0530

    Added solution - LeetHub

--- Replace all vowels in the String with the * Character [Private][UnPublished] - GFG/replace-all-vowels-in-the-string-with-the-character-private-un-published.java
+++ Replace all vowels in the String with the * Character [Private][UnPublished] - GFG/replace-all-vowels-in-the-string-with-the-character-private-un-published.java
@@ -1,33 +1,44 @@
+// { Driver Code Starts
+//Initial Template for Java
 import java.util.*;
 import java.io.*;
 
-public class Main{
+class Main{
     
     public static void main(String args[]){
-        
         Scanner scan=new Scanner(System.in);
         
         int t=scan.nextInt();
         
         while(t-->0){
-        String m=scan.next();
-        
-        StringBuffer ans=new StringBuffer();
-        
+            
+            String m=scan.next();
+            
+            GFG ob=new GFG();
+            
+            System.out.println(ob.replaceVowels(m));
+            
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GFG{
+    
+    String replaceVowels(String m){
+    StringBuffer ans=new StringBuffer("");
         for(int i=0;i<m.length();++i){
-            
             char ch=Character.toLowerCase(m.charAt(i));
-            
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            if(ch=='a'||ch=='e'||ch=='o'||ch=='i'||ch=='u'){
                 ans.append("*");
             }
             else{
                 ans.append(m.charAt(i));
             }
-          
         }
-        System.out.println(ans);
-        
-        }
+        return ans.toString();
     }
 }
+
