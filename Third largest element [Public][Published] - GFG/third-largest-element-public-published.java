f24ca65c51399e8ec3d0a30a56ece71712c0b86e
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-05-02 15:15:21 +0530

    Added solution - LeetHub

---
+++ Third largest element [Public][Published] - GFG/third-largest-element-public-published.java
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class ThirdLargestElement
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n =sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			Solution g = new Solution();
+			System.out.println(g.thirdLargest(arr,n));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+class Solution
+{
+    int thirdLargest(int arr[], int n)
+    {
+	    // Your code here
+	      if(n<3){
+      return -1;
+  }
+  else{
+     Arrays.sort(arr);
+      return arr[n-3];
+    }
+}
+}
