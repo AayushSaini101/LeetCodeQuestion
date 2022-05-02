4988b91d85a773e23fa300b499fe6014b93b6bfa
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-05-02 16:07:31 +0530

    Added solution - LeetHub

--- Replace all vowels in the String with the * Character [Private][UnPublished] - GFG/replace-all-vowels-in-the-string-with-the-character-private-un-published.java
+++ Replace all vowels in the String with the * Character [Private][UnPublished] - GFG/replace-all-vowels-in-the-string-with-the-character-private-un-published.java
@@ -1,45 +1,42 @@
 // { Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 import java.util.*;
 import java.io.*;
 
-class Main{
-    
-    public static void main(String args[]){
-        Scanner scan=new Scanner(System.in);
-        
-        int t=scan.nextInt();
-        
-        while(t-->0){
-            
-            String m=scan.next();
-            
-            GFG ob=new GFG();
-            
+class Main {
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+
+        while (t-- > 0) {
+
+            String m = scan.next();
+
+            GFG ob = new GFG();
+
             System.out.println(ob.replaceVowels(m));
-            
         }
     }
 }// } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class GFG{
-    
-    String replaceVowels(String m){
-          StringBuffer ans=new StringBuffer("");
-        for(int i=0;i<m.length();++i){
-            char ch=Character.toLowerCase(m.charAt(i));
-            if(ch=='a'||ch=='e'||ch=='o'||ch=='i'||ch=='u'){
+class GFG {
+
+String replaceVowels(String m) {
+        StringBuffer ans = new StringBuffer("");
+        for (int i = 0; i < m.length(); ++i) {
+            char ch = Character.toLowerCase(m.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'o' || ch == 'i' || ch == 'u') {
                 ans.append("*");
-            }
-            else{
+            } else {
                 ans.append(m.charAt(i));
             }
         }
-        
-        return ans.toString();
-    }
-}
 
+        return ans.toString();
+}
+}
