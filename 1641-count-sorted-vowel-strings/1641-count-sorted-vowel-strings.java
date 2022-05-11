0fe21059fdbb298907ff469df39f581a3c5a8632
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-05-11 18:40:18 +0530

    Time: 105 ms (10.07%), Space: 41.2 MB (23.34%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,40 +1,24 @@
 class Solution {
+    public int find(int n,int vowel){
+        if(n==0){
+           return 1;
+        }
+        else
+         if(vowel==0){
+             return 0;
+         }
+        
+        int answer=0;
+          
+        answer+=find(n-1,vowel)+find(n,vowel-1);
+        
+        return answer;
+        
+    }
     public int countVowelStrings(int n) {
+          
+         int answer=find(n,5);
         
-        if(n==1){
-            return 5;
-            
-        }
-        else{
-            int dp[][]=new int[n+1][6];
-             int sum=0;
-            for(int i=1;i<=5;++i){
-                dp[1][i]=5-(i-1);
-                
-            }
-            if(n==2){
-                return 15;
-            }
-            //Starting filling the table from 2 length
-           
-            for(int i=2;i<=n;++i){
-                for(int j=5;j>=0;--j){
-                    if(j==5){
-                        dp[i][j]=dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j]=dp[i-1][j]+dp[i][j+1];
-                    }
-                    
-                  
-                }
-            }
-            for(int i=1;i<=5;++i){
-                sum+=dp[n-1][i];
-            }
-            return sum;
-            
-        }
-        
+        return answer;
     }
 }
