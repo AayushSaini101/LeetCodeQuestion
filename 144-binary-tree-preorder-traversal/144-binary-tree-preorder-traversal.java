e8b312378cd333fc525372f0b6a0c03a0afd1c57
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-06-14 22:23:31 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (35.69%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+         TreeNode reference=root;
+        
+        List<Integer>lst=new ArrayList<>();
+        
+        while(reference!=null){
+            
+            if(reference.left==null){
+              
+                lst.add(reference.val);
+                
+                reference=reference.right;
+                
+            }
+            else{
+                
+                //Move to the left side of the node 
+                
+                TreeNode temp=reference.left;
+            
+                //This is also formation of the preprocessing the values 
+                
+                while(temp!=null&&temp.right!=null&&temp.right!=reference){
+                    
+                    temp=temp.right;
+                }
+                
+                if(temp.right!=reference){
+                    lst.add(reference.val);
+                    temp.right=reference;
+                    
+                    reference=reference.left;
+                
+                }
+                else{
+                    temp.right=null;
+                    
+                    reference=reference.right;
+                   
+                    
+                    
+                }
+                
+            }
+        }
+        
+        return lst;   
+    }
+}
