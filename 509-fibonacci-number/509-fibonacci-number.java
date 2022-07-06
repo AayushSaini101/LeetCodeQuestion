95119aad7f1ba90e32cf71f0ec908d6a0c2c4224
Zack_Aayush <60972989+AayushSaini101@users.noreply.github.com>
2022-07-06 14:28:12 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (17.51%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,14 +1,19 @@
 class Solution {
     //using the conecpet of the matrix multiplication 
     
-    public static int[][] multiply(int mat1[][],int mat2[][]){
+    public static void multiply(int mat1[][],int mat2[][]){
         
     int res[][]=new int[2][2];
     res[0][0] = mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
 	res[0][1] = mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
 	res[1][0] = mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0];
 	res[1][1] = mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1];
-    return res; 
+    mat1[0][0]=res[0][0];
+    mat1[0][1]=res[0][1];
+    mat1[1][0]=res[1][0];
+    mat1[1][1]=res[1][1];
+        
+    return ;
         
     }
     public static int[][] multiply(int n){
@@ -21,12 +26,12 @@ class Solution {
             
             if(n%2==1){
             
-               answer=multiply(answer,original);
+               multiply(answer,original);
                 // System.out.println(answer[0][0]+" "+answer[0][1]+" "+answer[1][0]+" "+answer[1][1]);
                 
             }
             
-            original=multiply(original,original);
+             multiply(original,original);
             
         
             n=n>>1;
